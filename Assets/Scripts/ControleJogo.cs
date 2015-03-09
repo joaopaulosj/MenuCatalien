5c3f3ca0c657247fbd06c504f8625a7ad435ae35
jpsjanuario <jpaulo_1914@hotmail.com>
2015-03-09 17:07:46 -0300

    retirada de getters e setters

--- Assets/Scripts/ControleJogo.cs
+++ Assets/Scripts/ControleJogo.cs
@@ -3,76 +3,16 @@ using System.Collections;
 
 public class ControleJogo : MonoBehaviour {
 	
-//	public int _idfase = 0 {get; set;}
-//	public int _qtdfases = 5 {get; set;}
-//	public int _idnivel = 0 {get; set;}
-//	public int _qtdniveis = 1 {get; set;}
-//	public int _pontuacao = 0 {get; set;}
+	public int _idfase = 0 {get; set;}
+	public int _qtdfases = 5 {get; set;}
+	public int _idnivel = 0 {get; set;}
+	public int _qtdniveis = 1 {get; set;}
+	public int _pontuacao = 0 {get; set;}
 	
-	private int _idfase = 0;
-	private int _qtdfases = 5;
-	private int _idnivel = 0;
-	private int _qtdniveis = 1;
-	private int _pontuacao = 0;
-	
-	#region - Start -
 	void Start () 
 	{
 		
 	}
-	#endregion
 	
-	#region - Getters/Setters -
-	
-	public void SetIdFase(int idfase)
-	{
-		_idfase = idfase;
-	}
-	
-	public int GetIdFase()
-	{
-		return _idfase;
-	}
-	
-	public void SetQtdFases(int qtdfases)
-	{
-		_qtdfases = qtdfases;
-	}
-	
-	public int GetQtdFases()
-	{
-		return _qtdfases;
-	}
-	
-	public void SetQtdNiveis(int qtdniveis)
-	{
-		_qtdniveis = qtdniveis;
-	}
-	
-	public int GetQtdNiveis()
-	{
-		return _qtdniveis;
-	}
-	
-	public void SetIdNivel(int idnivel)
-	{
-		_idnivel = idnivel;
-	}
-	
-	public int GetIdNivel()
-	{
-		return _idnivel;
-	}
-	
-	public void SetPontuacao(int pontuacao)
-	{
-		_pontuacao = pontuacao;
-	}
-	
-	public int GetPontuacao()
-	{
-		return _pontuacao;
-	}
-	#endregion
 	
 }
