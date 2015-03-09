44d805dd8dca6f8a96a7ad54a9fdbf63c60ddf82
jpsjanuario <jpaulo_1914@hotmail.com>
2015-03-09 14:19:40 -0300

    Update Pontuacao.cs

--- Assets/Scripts/Pontuacao.cs
+++ Assets/Scripts/Pontuacao.cs
@@ -6,6 +6,7 @@ public class Pontuacao {
 
 	public string Nome {get;set;}
 	public int Pontos {get;set;}
-    public DateTime Data {get;set;}
+    	public DateTime Data {get;set;}
+    	public int IdFase {get;set;}
 
 }
